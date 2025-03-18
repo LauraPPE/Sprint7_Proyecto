ee0944a41ef0c4f8be7b958574ad9308c1b3609c
Quica <escobarcatalina@gmail.com>
2025-03-18 08:43:00 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+df = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header('Vehicles US')
+
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(df, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
